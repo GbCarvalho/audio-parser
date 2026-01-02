847ca85dcfda00b6fab2672c584e8513d416a62d
Gabriel <braga.gabrielcarvalho@gmail.com>
2026-01-02 10:44:58 -0300

    Fix media playback control by adding optional chaining and refactoring click handlers

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,7 @@ import { useTemplateRef } from 'vue'
 const media = useTemplateRef('audio')
 
 const moveTo = (time) => {
-  media.value.currentTime = time
+  media.value?.currentTime = time
 }
 
 const data = await import('@/assets/data.json');
@@ -15,6 +15,12 @@ const getSeconds = (seconds: number) => {
 const getMinutes = (seconds: number) => {
   return Math.floor(seconds / 60).toString().padStart(2, '0');
 }
+
+const onTextClick = (start: number) => {
+  if(!media.value) return;
+  media.value?.currentTime = start;
+  media.value?.play();
+}
 </script>
 
 <template>
@@ -23,7 +29,7 @@ const getMinutes = (seconds: number) => {
       <source src="https://github.com/Azure-Samples/cognitive-services-speech-sdk/raw/master/scenarios/call-center/sampledata/Call6_mono_16k_az_apply_loan.wav"/>
     </audio>
     <div v-for="segment in data.segments" :key="segment.start">
-      <a @click.prevent="moveTo(segment.start)">[{{ getMinutes(segment.start) }}:{{ getSeconds(segment.start) }}]</a> <a class="cursor-pointer" v-for="word in segment.words" :key="word.word" @click.prevent="moveTo(word.start)" >{{ word.word + " " }}</a>
+      <a @click.prevent="onTextClick(segment.start)">[{{ getMinutes(segment.start) }}:{{ getSeconds(segment.start) }}]</a> <a class="cursor-pointer" v-for="word in segment.words" :key="word.word" @click.prevent="moveTo(word.start)" >{{ word.word + " " }}</a>
     </div>
   </div>
 </template>
